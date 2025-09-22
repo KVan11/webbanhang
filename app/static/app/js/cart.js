5c633050c0adcbbc24428e8a5918614a33a22bda
KVan11 <nguyenvan2005ty@gmail.com>
2025-09-22 17:22:41 +0700

    add item from home

---
+++ app/static/app/js/cart.js
@@ -0,0 +1,14 @@
+var updateBtns = document.getElementsByClassName('update-cart')
+for (i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function () {
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId:', productId, 'action:', action)
+        console.log('user:', user)
+        if (user === 'AnonymousUser') {
+            console.log('Not logged in')
+        } else {
+            console.log('Logged in, success add')
+        }
+    });
+}
