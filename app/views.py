564c918cbf2c4d45be7f824b170d9682723da326
KVan11 <nguyenvan2005ty@gmail.com>
2025-09-23 16:12:27 +0700

    logout

--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 from django.http import JsonResponse
 import json
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 def register(request):
     form = CreateUserForm()
@@ -13,9 +15,23 @@ def register(request):
             form.save()
     context = {'form': form}
     return render(request, 'app/register.html', context)
-def login(request):
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username OR password is incorrect!')
     context = {}
     return render(request, 'app/login.html', context)
+def logoutPage(request):
+    logout(request)
+    return redirect('login')
 def home(request):
     if request.user.is_authenticated:
         customer = request.user.customer
