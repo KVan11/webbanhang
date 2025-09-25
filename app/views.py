286e563475b469aca7eec3cceca66f512000e3c3
KVan11 <nguyenvan2005ty@gmail.com>
2025-09-25 22:42:52 +0700

    update ver1

--- app/views.py
+++ app/views.py
@@ -113,3 +113,18 @@ def category(request):
         products = Product.objects.filter(category__slug=active_category)
     context = {'categories': categories, 'products': products, 'active_category': active_category}
     return render(request, 'app/category.html', context)
+def detail(request):
+    if request.user.is_authenticated:
+        customer = request.user
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        cartItems = order['get_cart_items']
+    id = request.GET.get('id', '')
+    products = Product.objects.filter(id=id)
+    categories = Category.objects.filter(is_sub=False)
+    context = {'items': items, 'order': order, 'cartItems': cartItems, 'categories': categories, 'products': products}
+    return render(request, 'app/detail.html', context)
